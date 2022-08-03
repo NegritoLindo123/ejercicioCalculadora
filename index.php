f532f4b2100f8cb37fc4774f3a8aeb79aad3df95
NegritoLindo123 <cc1001447@gmail.com>
2022-08-03 14:48:48 -0500

    Se crearon las funciones para sumar y restar, y el metodo para ejecutar la operacion e imprimir el resultado

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
 
-    class Calculadora
+    class Calculadora {
+
+        //FUNCIÓN PARA SUMAR DOS NUMEROS
+        function sumar($numero1,$numero2){
+
+            $suma = $numero1 + $numero2;
+            $operacion = "Suma";
+
+            mostrarResultado($operacion,$suma);
+
+        }
+
+        //FUNCIÓN PARA RESTAR DOS NUMEROS
+        function restar($numero1,$numero2){
+
+            $resta = $numero1 - $numero2;
+            $operacion = "Resta";
+
+            mostrarResultado($operacion,$resta);
+
+        }
+
+        //METODO QUE VA A RETORNAR EL RESULTADO DE LA OPERACIÓN
+        function mostrarResultado($operacion,$resultado){
+
+            echo "El resultado de la ".$operacion." es: ".$resultado;
+
+        }
+
+    }
